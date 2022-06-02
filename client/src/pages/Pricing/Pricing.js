da6afdc15bd18fe1800e376204bc713dc56883af
danil <minakonn9shkoni@gmail.com>
2022-06-02 15:15:59 +0300

    Add correct

---
+++ client/src/pages/Pricing/Pricing.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './Pricing.module.scss'
+
+export const Pricing = (props) => {
+  return (
+
+    <div className={styles.pricingMain}>
+      <div className={styles.pricingContainer}>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+
+      </div>
+
+    </div>
+  )
+}
+
+
+export default Pricing;
